fd727d1e5cf5873123ded827740aac8be1beee8b
Sana Monov <sanamonov@gmail.com>
2017-02-23 15:28:25 -0500

    index changed

--- web/index.app.php
+++ web/index.app.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require '../vendor/autoload.php';
+require '../app/Config.php';
 require '../app/routing.php';
 require '../app/Router.php';
 $router = new Router($routing);
