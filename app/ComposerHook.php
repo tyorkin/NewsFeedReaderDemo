431d0e486c8b784cddd1c3941a9259f295c0b076
Alexander Monov <sanamonov@gmail.com>
2017-02-23 06:02:33 -0700

    composer hook updated

--- app/ComposerHook.php
+++ app/ComposerHook.php
@@ -1,22 +1,29 @@
 <?php
 
+use Composer\Script\Event;
 
 class ComposerHook
 {
-    public static function generateConfig()
+    public static function generateConfig(Event $event)
     {
+        $red ='\033[0;31m';
+        if (!is_file(__DIR__."/../build.ini")) {
+            $event->getIO()->writeError("<error>File build.ini not found!</error>");
+            die;
+        }
         $iniArray = parse_ini_file(__DIR__."/../build.ini");
         $configTemplateFile = __DIR__."/Config.php.dist";
         $configFile = __DIR__."/Config.php";
 
         if (!copy ($configTemplateFile, $configFile)) {
-            echo "Error generating config file\n";
+            $event->getIO()->writeError("<error>Config file not generated!</error>");
             die;
         }
         $file_contents = file_get_contents($configFile);
         foreach ($iniArray as $name => $value) {
             $file_contents = str_replace("%{$name}%",$value, $file_contents);
         }
+
         file_put_contents($configFile, $file_contents);
     }
 }
